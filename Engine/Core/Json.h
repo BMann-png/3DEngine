8eecd30c62dbbbb91f4f34d5e214d088f63ce958
BMann-png <bmann@student.neumont.edu>
2021-11-16 16:15:48 -0700

    Engine PRoject Added

---
+++ Engine/Core/Json.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "document.h"
+#include "Math/MathTypes.h"
+#include "SDL.h"
+#include <string>
+#include <vector>
+
+#define JSON_READ(value, data) pbls::json::Get(value, #data, data)
+
+namespace pbls
+{
+	namespace json
+	{
+		bool Load(const std::string& filename, rapidjson::Document& document);
+		bool Get(const rapidjson::Value& value, const std::string& name, int& key);
+
+		bool Get(const rapidjson::Value& value, const std::string& name, float& data);
+		bool Get(const rapidjson::Value& value, const std::string& name, bool& data);
+		bool Get(const rapidjson::Value& value, const std::string& name, std::string& data);
+		bool Get(const rapidjson::Value& value, const std::string& name, glm::vec2& data);
+		bool Get(const rapidjson::Value& value, const std::string& name, glm::vec3& data);
+		bool Get(const rapidjson::Value& value, const std::string& name, glm::vec4& data);
+
+
+		bool Get(const rapidjson::Value& value, const std::string& name, std::vector<std::string>& data);
+		bool Get(const rapidjson::Value& value, const std::string& name, std::vector<int>& data);
+	}
+}
